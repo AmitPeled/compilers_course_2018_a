462a4f782d59ae8fe97f3a2a0ac965f23b94f548
Noam Blagovsky <noam7700@gmail.com>
2019-02-17 10:27:04 +0200

    garbage

--- src/testRunDemo.bat
+++ src/testRunDemo.bat
@@ -14,20 +14,20 @@ break>test_out13.txt
 break>test_out14.txt
 break>test_out15.txt
 break>test_out16.txt
-javac homework2.java
-java homework2<tree1.txt>test_out1.txt
-java homework2<tree2.txt>test_out2.txt
-java homework2<tree3.txt>test_out3.txt
-java homework2<tree4.txt>test_out4.txt
-java homework2<tree5.txt>test_out5.txt
-java homework2<tree6.txt>test_out6.txt
-java homework2<tree7.txt>test_out7.txt
-java homework2<tree8.txt>test_out8.txt
-java homework2<tree9.txt>test_out9.txt
-java homework2<tree10.txt>test_out10.txt
-java homework2<tree11.txt>test_out11.txt
-java homework2<tree12.txt>test_out12.txt
-java homework2<tree13.txt>test_out13.txt
-java homework2<tree14.txt>test_out14.txt
-java homework2<tree15.txt>test_out15.txt
-java homework2<tree16.txt>test_out16.txt
+javac homework3.java
+java homework3<tree1.txt>test_out1.txt
+java homework3<tree2.txt>test_out2.txt
+java homework3<tree3.txt>test_out3.txt
+java homework3<tree4.txt>test_out4.txt
+java homework3<tree5.txt>test_out5.txt
+java homework3<tree6.txt>test_out6.txt
+java homework3<tree7.txt>test_out7.txt
+java homework3<tree8.txt>test_out8.txt
+java homework3<tree9.txt>test_out9.txt
+java homework3<tree10.txt>test_out10.txt
+java homework3<tree11.txt>test_out11.txt
+java homework3<tree12.txt>test_out12.txt
+java homework3<tree13.txt>test_out13.txt
+java homework3<tree14.txt>test_out14.txt
+java homework3<tree15.txt>test_out15.txt
+java homework3<tree16.txt>test_out16.txt
